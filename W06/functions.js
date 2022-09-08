53902082f3c730df3d67bcf77d55446fdf402620
Ran463 <68038662+Ran463@users.noreply.github.com>
2022-09-08 10:34:20 +1000

    Add files via upload

---
+++ W06/functions.js
@@ -0,0 +1,20 @@
+var emailAddress = new Vue({
+  el: '#emailAddress',
+  data: {
+    emailInput: '',
+  }
+});
+
+var password = new Vue({
+  el: '#password',
+  data: {
+    passwordInput: '',
+  }
+});
+
+var checkedColours = new Vue ({
+  el: '#checkbox',
+  data: {
+    checkedColoursInput: []
+  }
+});
